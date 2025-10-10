8fcdd862db39527e319916c3fc6d3e90b38b5eb9
Arthur Amaral <arthuramaral6@gmail.com>
2025-10-10 07:08:36 -0300

    config db_connection_handler with test

---
+++ src/models/settings/connection_test.py
@@ -0,0 +1,14 @@
+from src.models.settings.db_connection_handler import db_connection_handler
+
+def test_connection_to_db():
+  assert db_connection_handler.get_connection() is None
+
+  db_connection_handler.connect()
+
+  conn = db_connection_handler.get_connection()
+  pragma_status = conn.execute("PRAGMA foreign_keys;").fetchone()[0]
+  
+    
+  assert conn is not None
+  assert pragma_status == 1
+ 
