281020ab52c2076d3ef081438766177807854ace
Arthur Amaral <arthuramaral6@gmail.com>
2025-12-03 07:10:14 -0300

    feat: implementing order routes

---
+++ src/main/routes/order_routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify, request
+from src.view.http_types.http_request import HttpRequest
+
+from src.main.composer.new_order_composer import new_order_composer
+from src.main.composer.get_order_composer import get_order_composer
+
+order_routes_bp = Blueprint('order_routes', __name__)
+
+@order_routes_bp.route('/order/new_order/<user_id>', methods=['POST'])
+def place_order(user_id):
+  http_request = HttpRequest(body=request.json, params={'user_id': user_id})
+  http_response = new_order_composer().handle(http_resquest=http_request)
+  return jsonify(http_response.body), http_response.status_code
+
+@order_routes_bp.route('/order/get_orders/<user_id>', methods=['GET'])
+def get_orders(user_id):
+  http_request = HttpRequest(params={'user_id': user_id})
+  http_response = get_order_composer().handle(http_request=http_request)
+  return jsonify(http_response.body), http_response.status_code
